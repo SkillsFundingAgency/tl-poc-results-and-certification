679206b7c44ca6697998f1f5dcdbf68cb1ccbc3c
Rajesh Gaddam <rajesh.gaddam@digital.education.gov.uk>
2020-05-29 13:16:32 +0100

    TLRC - 921 - Technical Story - POC for Registrations file (Remaining Tasks) - TLRC - 959 - Dev - Created Tables relating to registrations

---
+++ Sfa.Poc.ResultsAndCertification.CsvHelper/Sfa.Poc.ResultsAndCertification.CsvHelper.Database/Tables/TqRegistrationPathway.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[TqRegistrationPathway]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL, 
+	[TqRegistrationId] INT NOT NULL,
+	[TqProviderId] INT NOT NULL,
+	[StartDate] DATETIME NOT NULL,
+	[EndDate] DATETIME NULL,
+	[Status] INT NOT NULL DEFAULT 1,
+	[CreatedOn] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+    [CreatedBy] NVARCHAR(50) NULL, 
+    [ModifiedOn] DATETIME2 NULL, 
+    [ModifiedBy] NVARCHAR(50) NULL,
+	CONSTRAINT [PK_TqRegistrationPathway] PRIMARY KEY ([Id]),
+	CONSTRAINT [FK_TqRegistrationPathway_TqRegistrationProfile] FOREIGN KEY ([TqRegistrationId]) REFERENCES [TqRegistrationProfile]([Id]),
+	CONSTRAINT [FK_TqRegistrationPathway_TqProvider] FOREIGN KEY ([TqProviderId]) REFERENCES [TqProvider]([Id])
+)
---
+++ Sfa.Poc.ResultsAndCertification.CsvHelper/Sfa.Poc.ResultsAndCertification.CsvHelper.Database/Tables/TqRegistrationProfile.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[TqRegistrationProfile]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL,    
+	[UniqueLearnerNumber] INT NOT NULL,
+	[Firstname] NVARCHAR(50) NULL, 
+	[Lastname] NVARCHAR(50) NULL, 
+	[DateofBirth] DATE NULL,
+	[CreatedOn] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+    [CreatedBy] NVARCHAR(50) NULL, 
+    [ModifiedOn] DATETIME2 NULL, 
+    [ModifiedBy] NVARCHAR(50) NULL,
+	CONSTRAINT [PK_TqRegistrationProfile] PRIMARY KEY ([Id])
+)
